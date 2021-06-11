4bd30e1b782de65487ee0d4b12c72b2c96e738a7
hectoralos <hectoralos@gmail.com>
2021-06-11 20:23:47 +0300

    +verbose

--- count.sh
+++ count.sh
@@ -1,2 +1,10 @@
 cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | wc -l
 cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep -v '"np"' | wc -l
+#echo "pr:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"pr"' | wc -l`
+echo "post:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"post"' | wc -l`
+echo "cnj:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"cnj' | wc -l`
+echo "num:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"num"' | wc -l`
+echo "det:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"det"' | wc -l`
+echo "prn:" `cat apertium-ben-hin.ben-hin.dix | grep '<e' | grep -v '<!--.*<e' | grep -v "i=yes" | grep '"prn"' | wc -l`
+
+
